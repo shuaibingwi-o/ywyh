2f6785ac0cb191fb04dd3c807b15be3b27775f62
shuaibingwi-o <shuaibingwi@hotmail.com>
2025-12-31 16:56:48 +0800

    Add BGP message format definition.

--- spf/bgp-ls.go
+++ spf/bgp-ls.go
@@ -1,3 +1,28 @@
-package spf 
- 
-// Placeholder for BGP-LS related implementations.
+package spf
+
+import (
+	"net"
+)
+
+type ipPrefix struct {
+	prefixLen uint8
+	ip        net.IP
+}
+
+type pathAttribute struct {
+	flags    uint8
+	typeCode uint8
+	length   uint16
+	value    []byte
+}
+
+type BGPUpdateMessage struct {
+	marker       [16]byte
+	len          uint16
+	myType       uint8
+	wrEntriesLen uint16
+	wrEntries    []ipPrefix
+	pathAttrsLen uint16
+	pathAttrs    []pathAttribute
+	nlriEntries  []ipPrefix
+}
--- spf/lsdb.go
+++ spf/lsdb.go
@@ -1,3 +1,13 @@
-package spf 
- 
+package spf
+
+import (
+	"sync"
+)
+
 // LSDB placeholder.
+// reconstructPath 从prev映射表中重建从起点到终点的完整路径
+
+// LSDB数据结构
+type LSDB struct {
+	mu sync.RWMutex
+}
